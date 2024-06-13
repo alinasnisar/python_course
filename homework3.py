cc0d125eab0bd746f7cd7bbda47e05d6e43bb13d
alinasnisar <snisarallina@gmail.com>
2024-06-13 13:42:44 +0300

    first commit

--- homework3.py
+++ homework3.py
@@ -1,9 +1,9 @@
-numbers_of_users = input("Введіть 4-х значне число")
+numbers_of_users = input("Введіть 5-ти значне число")
 number = int(numbers_of_users)
 
 digit1 = number % 10
 digit2 = (number % 100) // 10
 digit3 = (number % 1000) // 100
-digit4 = number // 1000
-
-print(digit1,digit2,digit3,digit4)
+digit4 = (number % 10000) // 1000
+digit5 = number // 10000
+print(digit1,digit2,digit3,digit4,digit5)
