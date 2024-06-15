9acf66e0c8c3da1434317654fd5df5b37030dd0d
alinasnisar <snisarallina@gmail.com>
2024-06-15 23:52:54 +0300

    first commit

---
+++ homework3.1.py
@@ -0,0 +1,14 @@
+value1 = float(input("Введіть число"))
+mathematical_operation = input("Введіть математичну операцію: +, -, /, *")
+value2 = float(input("Введіть число"))
+
+if mathematical_operation == "+":
+    result = value1 + value2
+elif mathematical_operation == "-":
+    result = value1 - value2
+elif mathematical_operation == "/" and value2 != 0:
+    result = value1 / value2
+elif mathematical_operation == "*":
+    result = value1 * value2
+
+print(f"{value1} {mathematical_operation} {value2} = {result}")
