977653e61834d9d8984c83c2b470669854e457cb
alinasnisar <snisarallina@gmail.com>
2024-06-17 01:38:35 +0300

    first commit

--- homework3.3.py
+++ homework3.3.py
@@ -9,8 +9,8 @@ elif len(list_of_values) % 2 == 0:
     result = [first_half, second_half]
 else:
     midpoint = len(list_of_values) // 2
-    first_half = list_of_values[midpoint:]
-    second_half = list_of_values[:midpoint]
+    first_half = list_of_values[:midpoint]
+    second_half = list_of_values[midpoint:]
     result = [first_half, second_half]
 
 print(f"{list_of_values} => {result}")
