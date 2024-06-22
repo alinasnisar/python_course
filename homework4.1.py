57759a344a9c1827a6bbefbc557a5dc8f6cbe8f3
alinasnisar <snisarallina@gmail.com>
2024-06-22 18:53:46 +0300

    first commit

---
+++ homework4.1.py
@@ -0,0 +1,8 @@
+listed_values = [31, 5, 8, 0, 3, 0, 51, 0, 3, 0, 1]
+
+zero_values = 0
+for i in range(len(listed_values)):
+    if listed_values[i] != 0:
+        listed_values[zero_values], listed_values[i] = listed_values[i], listed_values[zero_values]
+        zero_values += 1
+print(listed_values)
