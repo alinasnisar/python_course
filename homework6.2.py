24792f9647adfe4b1068679050fadca7086a9c95
alinasnisar <snisarallina@gmail.com>
2024-07-09 20:30:52 +0300

    first commit

---
+++ homework6.2.py
@@ -0,0 +1,22 @@
+seconds = int(input("Введіть секунди: "))
+
+if not isinstance(seconds, int) or seconds < 0 or seconds >= 8640000:
+    print("Необхідне ввести ціле число, яке менше ніж 8640000.")
+else:
+    days = seconds // 86400
+    remaining_seconds = seconds % 86400
+
+    hours = remaining_seconds // 3600
+    remaining_seconds %= 3600
+
+    minutes = remaining_seconds // 60
+    seconds = remaining_seconds % 60
+
+    days_str = str(days).zfill(2)
+    hours_str = str(hours).zfill(2)
+    minutes_str = str(minutes).zfill(2)
+    seconds_str = str(seconds).zfill(2)
+
+    time_str = f"{days_str} днів, {hours_str}:{minutes_str}:{seconds_str}"
+
+    print(f"Введений час: {time_str}")
