05fb6f5fa8e27be3c03df429ca28ae3764370cf3
alinasnisar <snisarallina@gmail.com>
2024-08-04 20:23:02 +0300

    2nd commit

---
+++ homework10.2.py
@@ -0,0 +1,12 @@
+def first_word(text):
+    """ Пошук першого слова """
+    return text.lstrip(" .,").split()[0].split('.')[0].rstrip(" .,")
+
+
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
+print('OK')
